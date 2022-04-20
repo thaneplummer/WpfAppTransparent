4e95a97a171363004588fc174ac7fe98157bb190
Thane <thane@appvizo.com>
2022-04-20 10:19:23 -0400

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppDelme
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            CommandBindings.Add(new CommandBinding(ApplicationCommands.Close,
+                new ExecutedRoutedEventHandler(delegate (object sender, ExecutedRoutedEventArgs args) { this.Close(); })));
+        }
+        public void DragWindow(object sender, MouseButtonEventArgs args)
+        {
+            DragMove();
+        }
+        public void ButtonClicked(object sender, RoutedEventArgs args)
+        {
+            MainWindow win2 = new MainWindow();
+            win2.ShowDialog();
+        }
+    }
+}
---
+++ ReadMe.md
@@ -0,0 +1,7 @@
+﻿### WPF Transparent Window
+
+Sample code to demonstrate how to make a transparent window in WPF. To be used for Movement Measures project.
+
+Blatant copy of blog post here: http://blogs.interknowlogy.com/2007/06/20/transparent-windows-in-wpf-2/
+
+
